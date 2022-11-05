8196f0e93270b7512fbe6b96fd03ed56cf58bd91
IsaM <70174157+IsabellaMenendez@users.noreply.github.com>
2022-11-04 20:52:11 -0500

    Add files via upload

--- fig2.py
+++ fig2.py
@@ -1,13 +1,17 @@
 #import plotly
 import plotly.express as px
 
-#create dataframe file  
+#create dataframe file
 import pandas as pd
 df = pd.read_csv("pop.csv")
 
-#split the data into category 
-fig = px.bar(df, x=['2018', '2019'], y=['Population - Female', 'Population - Male'],
-	labels=dict(x="Year"), title = "Population of Illinois 2018-2019")
+#split the data into category
+fig = px.bar(df, x=['2018', '2019'], y=['Population - Female', 'Population - Male'])
 
+fig.update_layout(
+                  title="Population of Chicago 2018-2019",
+                  xaxis_title="Year",
+                  yaxis_title="Population",
+                  legend_title="Key")
 #display graph
 fig.show()
--- fig3.py
+++ fig3.py
@@ -7,9 +7,13 @@ df = pd.read_csv("pop.csv")
 
 
 fig = px.bar(df, x=['2018', '2019'], y=['Population - Female', 'Population - Male'],
-	barmode = 'group', #split the data into two bars
-	labels=dict(x="Year"), title = "Population of Illinois 2018-2019")
+             barmode = 'group') #split the data into two bars
+
+fig.update_layout(
+                  title="Population of Chicago 2018-2019",
+                  xaxis_title="Year",
+                  yaxis_title="Population"
+                  legend_title="Key")
 
 #display graph
 fig.show()
-
